a58217e2c2f6bd0358fb4ab7c6c025df7d2f1ee4
mherhay93 <mherhay93@gmail.com>
2023-07-11 22:56:36 +0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+import {
+    Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+    const isDarkMode = useColorScheme() === 'dark';
+    
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+    
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}>
+                <View style={styles.container}>
+                    <Text>Hello World</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems:'center',
+        justifyContent:'center',
+    }
+});
+
+export default App;
